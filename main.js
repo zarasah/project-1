89c3bcf5f413309647801aaadd8a9a48a60ec76c
zarasah <zarinasah1989@gmail.com>
2023-01-25 15:27:41 +0400

    add featured

--- main.js
+++ main.js
@@ -2,38 +2,44 @@ const data = [
     {
         name: 'Stand Collar Short Down Jacket',
         price: 700,
-        img: 'images/1.jpg'
+        img: 'images/1.jpg',
+        id: 0
     },
     {
         name: 'Down jacket with high collar',
         price: 870,
-        img: 'images/2.jpg'
+        img: 'images/2.jpg',
+        id: 1
     },
     {
         name: 'Turtleneck with long sleeves',
         price: 450,
-        img: 'images/3.jpg'
+        img: 'images/3.jpg',
+        id: 2
     },
     {
         name: 'Long sweater',
         price: 300,
-        img: 'images/4.jpg'
+        img: 'images/4.jpg',
+        id: 3
     },
     {
         name: 'Shopper tote bag',
         price: 1400,
-        img: 'images/5.jpg'
+        img: 'images/5.jpg',
+        id: 4
     },
     {
         name: 'Compact purse with shoulder strap',
         price: 250,
-        img: 'images/6.jpg'
+        img: 'images/6.jpg',
+        id: 5
     }
 ]
 
 const div = document.querySelector('.main__right');
 
-function addProduct(img, name, price) {
+function addProduct(img, name, price, id) {
     const newPrise = '$ ' + price + ' USD'
     const newDiv = document.createElement('div');
     const productName = document.createTextNode(name);
@@ -51,6 +57,7 @@ function addProduct(img, name, price) {
 
     addDiv.classList.add('hidden');
     newDiv.classList.add('grid-item');
+    newDiv.id = id;
     newDiv.append(productImg, newH3, newP, addDiv);
     div.appendChild(newDiv);
 
@@ -62,22 +69,58 @@ function addProduct(img, name, price) {
         addDiv.classList.remove('show');
     })
 
+    newDiv.addEventListener('click', (event) => {
+        const tar = event.target;
+        const index = tar.closest('div').id;
+        if(index) {
+            let number = +index;
+            fretured(data[number]);
+        }
+    })
+
     addDiv.addEventListener('click', addToCart);
 }
 
-data.forEach(({img, name, price}) => {
-    addProduct(img, name, price);
+data.forEach(({img, name, price, id}) => {
+    addProduct(img, name, price, id);
 })
 
 let counter = 0;
 
 function addToCart() {
     counter++;
-    
+
     const countDiv = document.querySelector('.counter') || document.createElement('div');
     countDiv.classList.add('counter');
     const basket = document.querySelector('.basket');
 
     countDiv.textContent = counter;
     basket.appendChild(countDiv);
+}
+
+function fretured(obj) {
+    const itemArray = document.querySelectorAll('.item');
+    const history = document.querySelector('.wrapper');
+    const newItem = document.createElement('div');
+    const itemR = document.createElement('div');
+    const itemL = document.createElement('div');
+    const image = document.createElement('img');
+    const p = document.createElement('p');
+
+    p.textContent = obj.name;
+    itemR.appendChild(p);
+
+    image.src = obj.img;
+    itemL.appendChild(image);
+
+    itemL.classList.add('history');
+    itemR.classList.add('history');
+    newItem.classList.add('item');
+
+    if (itemArray.length > 2) {
+        itemArray[0].parentNode.removeChild(itemArray[0]);        
+    }
+
+    newItem.append(itemL, itemR);
+    history.appendChild(newItem);
 }
